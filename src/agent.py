2b804b65293461d3dcaa9ec23a746446aaa71a49
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-12-05 18:29:51 -0300

    Update agent.py

--- src/agent.py
+++ src/agent.py
@@ -13,6 +13,10 @@ import torch.optim as optim
 import yaml
 import os
 
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+
 # folder to load config file
 CONFIG_PATH = "../config/"
 
@@ -26,6 +30,13 @@ def load_config(config_name):
 param = load_config("main_config.yaml")
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+config = {
+    "l1": tune.sample_from(lambda _: 2**np.random.randint(2, 9)),
+    "l2": tune.sample_from(lambda _: 2**np.random.randint(2, 9)),
+    "lr": tune.loguniform(1e-4, 1e-1),
+    "batch_size": tune.choice([2, 4, 8, 16])
+}
 
 class Agent():
     """Interacts with and learns from the environment."""
