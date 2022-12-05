f5094325872e753de8415414a8fcc9e66b9e7877
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-12-05 17:49:01 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Source ROS and Catkin workspaces
+source /opt/ros/noetic/setup.bash
+echo "source /ws/devel/setup.bash" >> ~/.bashrc
+source /ws/devel/setup.bash
+
+echo "Sourced Catkin workspace!"
+
+# Set environment variables
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(rospack find motion-rl)/models
+
+roslaunch motion-rl start.launch
+# Execute the command passed into this entrypoint
+exec "$@"
