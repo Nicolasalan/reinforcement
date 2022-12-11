c60f6fe2d5173931c71dc68bf320741ceeaa4ef3
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-12-11 20:01:40 -0300

    Update environment.py

--- src/environment.py
+++ src/environment.py
@@ -186,7 +186,7 @@ class Env():
           data = None
           while data is None:
                try:
-                    data = rospy.wait_for_message('/base_scan_front', LaserScan, timeout=5)
+                    data = rospy.wait_for_message('base_scan_front', LaserScan, timeout=5)
                     rospy.spin()
                except:
                     pass
