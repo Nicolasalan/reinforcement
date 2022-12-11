3ecd762f0d6e7ea044dab2075767d2a24c8bac63
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-12-11 01:36:35 -0300

    Update ddpg.py

--- src/ddpg.py
+++ src/ddpg.py
@@ -63,10 +63,9 @@ def ddpg(n_episodes, print_every, max_t, score_solved):
           for t in range(max_t):# escolha uma ação para cada agente
                print('timestep: ' + str(t))
                print(states)
-               action = agent.action(states)    
-               actions = [(action[0] + 1) / 2, action[1]]        
-               #actions[0] = agent.action(states, 0.0, 1.0)                   # selecione uma ação
-               #actions[1] = agent.action(states,0.5, 1.5)
+               actions = agent.action(states)            
+               actions[0] = agent.action(states, 0.0, 1.0)                   # selecione uma ação
+               actions[1] = agent.action(states,0.5, 1.5)
                next_states, rewards, dones, target = env.step(actions, past_action) # envia todas as ações ao ambiente
                
                # salva a experiência no buffer de repetição, executa a etapa de aprendizado em um intervalo definido
