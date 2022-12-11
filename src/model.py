af4ac2f82a65be1dabe6799cbc04d01417b42737
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-12-11 01:54:14 -0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -47,7 +47,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fcs1_units=800, fc2_units=600):
         """Initialize parameters and build model.
         Params
         ======
