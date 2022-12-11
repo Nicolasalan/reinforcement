d3dc81f20a3c6fb2f0c565934b04102a5b51df16
Nicolas Alan <nicolasalangrotti03@gmail.com>
2022-12-11 05:28:46 -0300

    Update environment.py

--- src/environment.py
+++ src/environment.py
@@ -222,7 +222,7 @@ class Env():
                
                object_state.pose.position.x = self.goal_x
                object_state.pose.position.y = self.goal_y
-               print("Goal x: ", self.goal_x, "Goal y: ", self.goal_y)
+               print("Goal x: ", -4, "Goal y: ", 5)
 
                object_state.pose.orientation.x = quaternion.x
                object_state.pose.orientation.y = quaternion.y
