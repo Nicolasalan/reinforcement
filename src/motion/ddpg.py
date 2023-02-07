6133933327cdc152689d1ae1f223c320f3686077
Nicolas Alan <nicolasalangrotti03@gmail.com>
2023-02-07 00:42:46 -0300

    create transfer learning model

--- src/motion/ddpg.py
+++ src/motion/ddpg.py
@@ -12,6 +12,8 @@ import psutil
 import rospy
 import numpy as np
 import os
+import torch.nn as nn
+
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 checkpoints_dir = os.path.join(script_dir, 'checkpoints')
@@ -157,8 +159,6 @@ def ddpg(n_episodes, print_every, max_t, score_solved, param, CONFIG_PATH, count
           scores = []                                                      # list of average scores of each episode
 
           num_resets = 0   
-          print(count)  
-          print(num_resets)    
 
           while num_resets < count:                                        # initialize score for each agent
                done = False       
@@ -186,6 +186,89 @@ def ddpg(n_episodes, print_every, max_t, score_solved, param, CONFIG_PATH, count
                cpu_usage = psutil.cpu_percent()
                rospy.logwarn('CPU and Memory               => usage: ' + str(cpu_usage) + '%, ' + str(psutil.virtual_memory().percent) + '%')
 
+     ## ====================== Transfer Learning ====================== ##
+
+     if param["TYPE"] == 3:
+          print('Transfer learning in simulation')
+
+          agent = Agent(state_size=state_dim, action_size=action_dim, random_seed=42, CONFIG_PATH=CONFIG_PATH)
+          env = Env(CONFIG_PATH)
+
+          agent.actor_local.load_state_dict(torch.load(param["model_actor"]))
+          agent.critic_local.load_state_dict(torch.load(param["model_critic"]))
+          
+          hidden_size = 256
+
+          # Freeze the pre-trained model's layers
+          for param in agent.actor_local.parameters():
+               param.requires_grad = False
+          for param in agent.critic_local.parameters():
+               param.requires_grad = False
+     
+          # Add new layers on top of the pre-trained models
+          new_actor = nn.Sequential(
+               agent.actor_local,
+               nn.Linear(hidden_size, hidden_size),
+               nn.Tanh(),
+               nn.Linear(hidden_size, action_dim)
+          )
+          new_critic = nn.Sequential(
+               agent.critic_local,
+               nn.Linear(hidden_size, hidden_size),
+               nn.Tanh(),
+               nn.Linear(hidden_size, 1)
+          )
+
+          # Replace the old models with the new models
+          agent.actor_local = new_actor
+          agent.critic_local = new_critic
+
+          scores_window = []                                               # average scores of the most recent episodes
+          scores = []                                                      # list of average scores of each episode                     
+
+          for i_episode in range(n_episodes+1):                            # initialize score for each agent
+               rospy.loginfo('Episode: ' + str(i_episode))
+               score = 0.0                
+               done = False
+
+               agent.reset()                                               # reset environment    
+               states = env.reset_env()                                    # get the current state of each agent
+
+               for t in range(max_t):   
+                         
+                    action = agent.action(states)                          # choose an action for each agent
+                    actions = [(action[0] + 1) / 2, action[1]]
+
+                    next_states, rewards, done, _ = env.step_env(actions)  # send all actions to the environment
+
+                    # save the experiment in the replay buffer, run the learning step at a defined interval
+                    agent.step(states, actions, rewards, next_states, done, t)
+
+                    states = next_states
+                    score += rewards
+                    if np.any(done):                                       # exit loop when episode ends
+                         break              
+                    
+                    scores_window.append(score)                            # save average score for the episode
+                    scores.append(score)                                   # save average score in the window  
+                         
+               cpu_usage = psutil.cpu_percent()
+               rospy.logwarn('CPU and Memory               => usage: ' + str(cpu_usage) + '%, ' + str(psutil.virtual_memory().percent) + '%')
+               
+               if i_episode % print_every == 0:
+                    rospy.logwarn('# ====== Episode: ' + str(i_episode) + ' Average Score: ' + str(np.mean(scores_window)) + ' ====== #')
+               
+               if i_episode % 1000 == 0:
+                    torch.save(agent.actor_local.state_dict(), os.path.join(checkpoints_dir, '{}_actor_checkpoint.pth'.format(i_episode)))
+                    torch.save(agent.critic_local.state_dict(), os.path.join(checkpoints_dir, '{}_critic_checkpoint.pth'.format(i_episode)))
+
+               if np.mean(scores_window) >= score_solved:
+                    rospy.logwarn('Environment solved in ' + str(i_episode) + ' episodes!' + ' Average Score: ' + str(np.mean(scores_window)))
+                    torch.save(agent.actor_local.state_dict(), os.path.join(checkpoints_dir, 'actor_checkpoint.pth'))
+                    torch.save(agent.critic_local.state_dict(), os.path.join(checkpoints_dir, 'critic_checkpoint.pth'))
+                    break
+
+          return scores
 if __name__ == '__main__':
      """Start training."""
      
